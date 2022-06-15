e90b407a8ad163f33810088129bc369f4cf658b8
+ <+>
2022-06-15 23:17:03 +0300

    commit tree 3bf8848a5b01a3067d30eb024f7d0cb396f50be0

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Add your code here
-function submitData( name, email ) {
+function submitData( name="Sam", email= "sam@sam.com") {
   return fetch( 'http://localhost:3000/users', {
       method: "POST",
       headers: {
